6831fc7275b374237d67ad5d5c7c7af308663d02
Larissa Cruz <larissacruz95@gmail.com>
2025-10-29 19:39:20 -0300

    Listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos ', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+            //.last()
+            //.fist()
+            //.eq(3)
+            .contains('Ajax Full-Zip Sweatshirt')
+            .click()
+
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+
+
+
+
+});
